ec279522708b341f2fffe914bc70b3ed9c41e2de
Владислав <morisonadd@mail.ru>
2021-10-04 15:06:04 +0500

    added slider and some styles for the 'our projects' page

--- KeyBoardSite/js/Scrypt.js
+++ KeyBoardSite/js/Scrypt.js
@@ -1,19 +1,18 @@
 "use strict"
 import addScrollHeader from './headerScroll.js';
 
-// const categoryNameCard = [
-//     'Мифические', 'Фантастика', 'Классические',
-//     'Постаппокалипсис', 'Marvel', 'DC', 'Времена года'
-// ];
+
 const API_cardProduct = './database.json';
 const API_cardDeliveryCompanies = './dataDeliveryCompanies.json';
+const API_cardDoneProject = './dataDoneProjects.json';
+
 $(document).ready(function (){
 
-    $('button').click(function(){
-        for (const key in this) {
-            $(this).click(getCardByCategory(this.key + 1))
-        }
-    })
+    // $('button').click(function(){
+    //     for (const key in this) {
+    //         $(this).click(getCardByCategory(this.key + 1))
+    //     }
+    // })
 
     addScrollHeader();
     async function getCard(callback, buttonClickedID){
@@ -39,6 +38,18 @@ $(document).ready(function (){
         addCardDelivery(cardData, parent);
         })
     };
+    async function getCardOurWork(callback){
+        await fetch(API_cardDoneProject)
+        .then(responce => responce.json())
+        .then(cardData => {
+
+
+        let parent = document.querySelector(".slider_card_work");
+
+        addCardWorkDone(cardData, parent);
+        callback();
+        })
+    };
 
         function addSlider(){
             $('.slider_card').slick({
@@ -50,45 +61,56 @@ $(document).ready(function (){
                 variableWidth: true
             });
         };
+        function addSliderWork(){
+            $('.slider_card_work').slick({
+                dots: false,
+                infinite: false,
+                speed: 500,
+                slidesToShow: 3,
+                centerMode: false,
+                variableWidth: true
+            });
+        };
+    getCardOurWork(addSliderWork);
     getCard(addSlider);
     getCardDelivery();
 
 });
 
-function getCardByCategory(id){
+// function getCardByCategory(id){
 
-    switch(id) {
-        case '1':
-            return button.addEventListener("click", function (){
-                getCard(addSlider,id)
-        })
-        case '2':
-            return button.addEventListener("click", function (){
-                getCard(addSlider,id)
-        })
-        case '3':
-            return button.addEventListener("click", function (){
-                getCard(addSlider,id)
-        })
-        case '4':
-            return button.addEventListener("click", function (){
-                getCard(addSlider,id)
-        })
-        case '5':
-            return button.addEventListener("click", function (){
-                getCard(addSlider,id)
-        })
-        case '6':
-            return button.addEventListener("click", function (){
-                getCard(addSlider,id)
-        })
-        case '7':
-            return button.addEventListener("click", function (){
-                getCard(addSlider,id)
-        })
+//     switch(id) {
+//         case '1':
+//             return button.addEventListener("click", function (){
+//                 getCard(addSlider,id)
+//         })
+//         case '2':
+//             return button.addEventListener("click", function (){
+//                 getCard(addSlider,id)
+//         })
+//         case '3':
+//             return button.addEventListener("click", function (){
+//                 getCard(addSlider,id)
+//         })
+//         case '4':
+//             return button.addEventListener("click", function (){
+//                 getCard(addSlider,id)
+//         })
+//         case '5':
+//             return button.addEventListener("click", function (){
+//                 getCard(addSlider,id)
+//         })
+//         case '6':
+//             return button.addEventListener("click", function (){
+//                 getCard(addSlider,id)
+//         })
+//         case '7':
+//             return button.addEventListener("click", function (){
+//                 getCard(addSlider,id)
+//         })
             
-    }
-}
+//     }
+// }
 
 
 function addCard(cards, parent, categoryID){
@@ -140,6 +162,27 @@ function addCardDelivery(cards, parent){
                     <p class="region_company">${cards[key].regionDelivery}</p>
                 </div>
             </div>
+        </div>
+        `;
+        
+        CardChild.innerHTML = cardDelivery;
+        parent.appendChild(CardChild);
+        
+    }
+}
+
+function addCardWorkDone(cards, parent){
+
+    for (const key in cards) {
+        let CardChild = document.createElement('div');
+        let cardDelivery = 
+        `
+        <div class="our_work_card" style="background: url(${cards[key].img})">
+            <p class="title_card_our_work">${cards[key].name}</p>
+            <div class="details">
+                <p class="detail_about_case">Подробнее про кейс</p> 
+            </div>
+            <a class="push_button" data-fancybox data-src="#modal${cards[key].popupID}" href="#">Смотреть кейс</a>
         </div>
         `;
         
