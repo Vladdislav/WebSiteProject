992ca2bfc65c9c7164163009fe21e549b10ed63f
Владислав <morisonadd@mail.ru>
2021-10-23 11:32:34 +0500

    some fix

--- PortfolioSite/app/js/main.js
+++ PortfolioSite/app/js/main.js
@@ -37,48 +37,82 @@ $(function(){
       
       
 
-      var wow = new WOW(
-        {
-          boxClass:     'wow',      // класс, скрывающий элемент до момента отображения на экране (по умолчанию, wow)
-          animateClass: 'animated', // класс для анимации элемента (по умолчанию, animated)
-          offset:       0,          // расстояние в пикселях от нижнего края браузера до верхней границы элемента, необходимое для начала анимации (по умолчанию, 0)
-          mobile:       true,       // включение/отключение WOW.js на мобильных устройствах (по умолчанию, включено)
-          live:         true,       // поддержка асинхронно загруженных элементов (по умолчанию, включена)
-          callback:     function(box) {
-            // функция срабатывает каждый раз при старте анимации
-            // аргумент box — элемент, для которого была запущена анимация
-          },
+  var wow = new WOW(
+    {
+      boxClass:     'wow',      // класс, скрывающий элемент до момента отображения на экране (по умолчанию, wow)
+      animateClass: 'animated', // класс для анимации элемента (по умолчанию, animated)
+      offset:       0,          // расстояние в пикселях от нижнего края браузера до верхней границы элемента, необходимое для начала анимации (по умолчанию, 0)
+      mobile:       true,       // включение/отключение WOW.js на мобильных устройствах (по умолчанию, включено)
+      live:         true,       // поддержка асинхронно загруженных элементов (по умолчанию, включена)
+      callback:     function(box) {
+        // функция срабатывает каждый раз при старте анимации
+        // аргумент box — элемент, для которого была запущена анимация
+      },
+    }
+  );
+  const allTagA = document.querySelectorAll('a[href]');
+
+      for(var i = 0; i < allTagA.length; i++){
+
+        if (allTagA[i].getAttribute('href').substr(1) == 'main'){
+
+          const main = allTagA[i];
+          main.addEventListener('click', function(event){
+
+            event.preventDefault();
+    
+            const scrollTargetMain = document.getElementById('main');
+    
+            const topOffset = 0;
+    
+            const elementPosition =  scrollTargetMain.getBoundingClientRect().top;
+    
+            const offsetPosition = elementPosition - topOffset;
+    
+            window.scrollBy({
+              top: offsetPosition,
+              behavior: 'smooth'
+            })
+          })
         }
-      );
+        else if (allTagA[i].getAttribute('href').substr(1) == 'about'){
 
+          const about = allTagA[i];
+          about.addEventListener('click', function(event){
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            event.preventDefault();
+    
+            const scrollTargetAbout = document.getElementById('about');
+            if (scrollTargetAbout.click){
+              $('a.header__top-item-link').addClass('header__top-item-link--active');
+            }
+    
+            const topOffset = 0;
+    
+            const elementPosition =  scrollTargetAbout.getBoundingClientRect().top;
+    
+            const offsetPosition = elementPosition - topOffset;
+    
+            window.scrollBy({
+              top: offsetPosition,
+              behavior: 'smooth'
+            })
+          })
+        }
+      }
+      
       wow.init();
       onScrollHeader();
 
-      // Initialise Carousel
-const logoCarousel = new Carousel(document.querySelector("#carusel"), {
+  // Initialise Carousel
+const logoCarousel = new Carousel(document.querySelector("#logoCarousel"), {
   friction: 0.83,
   Dots: false,
   on: {
     change: (carousel, to) => {
       // Clear active elements
       document
-        .querySelectorAll("#carusel .is-active, #caruselBar .is-active")
+        .querySelectorAll("#logoCarousel .is-active, #logoBar .is-active")
         .forEach((el) => {
           el.classList.remove("is-active");
         });
@@ -86,7 +120,7 @@ const logoCarousel = new Carousel(document.querySelector("#carusel"), {
       // Mark current elements as active
       document
         .querySelectorAll(
-          `#carusel [data-for="${to}"], #caruselBar [data-for="${to}"]`
+          `#logoCarousel [data-for="${to}"], #logoBar [data-for="${to}"]`
         )
         .forEach((el) => {
           el.classList.add("is-active");
@@ -96,7 +130,7 @@ const logoCarousel = new Carousel(document.querySelector("#carusel"), {
 });
 
 // Make links clickable
-document.getElementById("caruselBar").addEventListener("click", (event) => {
+document.getElementById("logoBar").addEventListener("click", (event) => {
   const index = event.target.dataset.for || -1;
 
   if (index > -1) {
